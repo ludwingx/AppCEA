93749fde34a43579133d47f8b40408633e999ea3
Ludwingx <ludwingytx@gmail.com>
2022-09-27 12:52:01 -0400

    0.0.5

--- Frontend/src/app/pages/garecepcion/crearar/crearar.module.ts
+++ Frontend/src/app/pages/garecepcion/crearar/crearar.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-imports: [CommonModule]
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
--- Frontend/src/app/pages/garecepcion/crearar/crearar.page.ts
+++ Frontend/src/app/pages/garecepcion/crearar/crearar.page.ts
@@ -15,19 +15,21 @@ export class CreararPage implements OnInit {
 
   ngOnInit() { 
     this.ListAtencion()
-    // this.ListMunicipios()
+    this.ListMunicipios()
   }
-  change(event){
-    console.log(event.detail.value); //INTRODUCIR EL VALUE EN UN OBJETO "DATE" Y ENVIARLO
-  }
+
   ListAtencion(){
     this.conexion.getdata("atencion.php/?aksi=list-tatencion").subscribe((data:any)=>{
       this.tatencion = data.listTatencion
     })
   }
-  // ListMunicipios(){
-  //   this.conexion.getdata("municipio.php/?aksi=list-municipio").subscribe((data:any)=>{
-  //     this.municipios = data.listMunicipio
-  //   })
-  // }
+  ListMunicipios(){
+    this.conexion.getdata("municipio.php/?aksi=list-municipio").subscribe((data:any)=>{
+      this.municipios = data.listMunicipios
+    })
+  }
+
+  change(event){
+    console.log(event.detail.value); //INTRODUCIR EL VALUE EN UN OBJETO "DATE" Y ENVIARLO
+  }
 }
